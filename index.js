3ef9e1dfc3a808bcaf26f2cf6a95e2d642602331
felipe-rosa94 <luisfeliperc1994@gmail.com>
2023-10-10 10:45:27 -0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const test = (text) => {
-    console.log('test: ', text)
-}
+const test = text => console.log(text)
+
+const isObjectEmpty = object => object && Object.keys(object).length === 0 && Object.getPrototypeOf(object) === Object.prototype
+
+const removeAccents = string => string.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+const cleanTextToNumbers = string => string.replace(/[^\d]+/g, '').trim()
 
 module.exports = {
-    test
+    test,
+    isObjectEmpty,
+    removeAccents,
+    cleanTextToNumbers
 }
