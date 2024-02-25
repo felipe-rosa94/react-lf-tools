64c3ac372da91326ec2b8f75be500746bdcd06f2
felipe-rosa94 <luisfeliperc1994@gmail.com>
2024-02-25 12:03:58 -0300

    version 1.0.17

--- index.js
+++ index.js
@@ -353,12 +353,12 @@ const maskPhone = phone => {
     }
 }
 
-const maskDate = data => {
+const maskDate = (data, format = '/') => {
     let v = data.replace(/\D/g, '').slice(0, 10)
     if (v.length >= 5) {
-        return `${v.slice(0, 2)}/${v.slice(2, 4)}/${v.slice(4)}`
+        return `${v.slice(0, 2)}${format}${v.slice(2, 4)}${format}${v.slice(4)}`
     } else if (v.length >= 3) {
-        return `${v.slice(0, 2)}/${v.slice(2)}`
+        return `${v.slice(0, 2)}${format}${v.slice(2)}`
     }
     return v
 }
